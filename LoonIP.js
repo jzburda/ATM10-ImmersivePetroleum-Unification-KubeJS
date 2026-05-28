983bdaef1f5f1e870101d1cc3b377dfd51dcdfcc
Loonessia <jzburda@gmail.com>
2026-05-28 03:53:52 -0500

    Create LoonIP.js

---
+++ LoonIP.js
@@ -0,0 +1,13 @@
+ServerEvents.tags('fluid', allthemods => {
+allthemods.add('c:crude_oil', 'immersivepetroleum:crudeoil')
+allthemods.add('c:fuels/crude_oil', 'immersivepetroleum:crudeoil')
+allthemods.add('c:diesel', 'immersivepetroleum:diesel')
+allthemods.add('c:lubricant', 'immersivepetroleum:lubricant')
+allthemods.add('c:kerosene', 'immersivepetroleum:kerosene')
+allthemods.add('c:gasoline', 'immersivepetroleum:gasoline')
+allthemods.add('c:naphtha', 'immersivepetroleum:naphtha')
+allthemods.add('c:lpg', 'immersivepetroleum:petroleum_gas')
+}
+)
+
+
